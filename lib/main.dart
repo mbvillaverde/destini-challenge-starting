131edd365742e41e2223630804b20e7974c6b5c3
Michael Villaverde <michaelv@qed.software>
2024-02-23 09:39:13 +0800

    update done to emoji

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:destini_challenge_starting/story_brain.dart';
 import 'package:flutter/material.dart';
 
-//DONE: Step 15 - Run the app and see if you can see the screen update with the first story. Delete this TODO if it looks as you expected.
+// ✅ Step 15 - Run the app and see if you can see the screen update with the first story. Delete this TODO if it looks as you expected.
 
 void main() => runApp(Destini());
 
@@ -14,7 +14,7 @@ class Destini extends StatelessWidget {
   }
 }
 
-//DONE: Step 9 - Create a new storyBrain object from the StoryBrain class.
+// ✅ Step 9 - Create a new storyBrain object from the StoryBrain class.
 StoryBrain storyBrain = StoryBrain();
 
 class StoryPage extends StatefulWidget {
@@ -26,7 +26,7 @@ class _StoryPageState extends State<StoryPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        //DONE: Step 1 - Add background.png to this Container as a background image.
+        // ✅ Step 1 - Add background.png to this Container as a background image.
         decoration: BoxDecoration(
           image: DecorationImage(
             image: AssetImage('images/background.png'),
@@ -43,7 +43,7 @@ class _StoryPageState extends State<StoryPage> {
                 flex: 12,
                 child: Center(
                   child: Text(
-                    //DONE: Step 10 - use the storyBrain to get the first story title and display it in this Text Widget.
+                    // ✅ Step 10 - use the storyBrain to get the first story title and display it in this Text Widget.
                     storyBrain.getStory(),
                     style: TextStyle(
                       fontSize: 25.0,
@@ -56,14 +56,14 @@ class _StoryPageState extends State<StoryPage> {
                 child: TextButton(
                   onPressed: () {
                     //Choice 1 made by user.
-                    //DONE: Step 18 - Call the nextStory() method from storyBrain and pass the number 1 as the choice made by the user.
+                    // ✅ Step 18 - Call the nextStory() method from storyBrain and pass the number 1 as the choice made by the user.
                     setState(() {
                       storyBrain.nextStory(1);
                     });
                   },
                   style: TextButton.styleFrom(backgroundColor: Colors.red),
                   child: Text(
-                    //DONE: Step 13 - Use the storyBrain to get the text for choice 1.
+                    // ✅ Step 13 - Use the storyBrain to get the text for choice 1.
                     storyBrain.getChoice1(),
                     textAlign: TextAlign.center,
                     style: TextStyle(
@@ -77,21 +77,21 @@ class _StoryPageState extends State<StoryPage> {
               ),
               Expanded(
                 flex: 2,
-                //DONE: Step 26 - Use a Flutter Visibility Widget to wrap this FlatButton.
-                //DONE: Step 28 - Set the "visible" property of the Visibility Widget to equal the output from the buttonShouldBeVisible() method in the storyBrain.
+                // ✅ Step 26 - Use a Flutter Visibility Widget to wrap this FlatButton.
+                // ✅ Step 28 - Set the "visible" property of the Visibility Widget to equal the output from the buttonShouldBeVisible() method in the storyBrain.
                 child: Visibility(
                   visible: storyBrain.buttonShouldBeVisible(),
                   child: TextButton(
                     onPressed: () {
                       //Choice 2 made by user.
-                      //DONE: Step 19 - Call the nextStory() method from storyBrain and pass the number 2 as the choice made by the user.
+                      // ✅ Step 19 - Call the nextStory() method from storyBrain and pass the number 2 as the choice made by the user.
                       setState(() {
                         storyBrain.nextStory(2);
                       });
                     },
                     style: TextButton.styleFrom(backgroundColor: Colors.blue),
                     child: Text(
-                      //DONE: Step 14 - Use the storyBrain to get the text for choice 2.
+                      // ✅ Step 14 - Use the storyBrain to get the text for choice 2.
                       storyBrain.getChoice2(),
                       textAlign: TextAlign.center,
                       style: TextStyle(
@@ -109,6 +109,6 @@ class _StoryPageState extends State<StoryPage> {
   }
 }
 
-//DONE: Step 24 - Run the app and try to figure out what code you need to add to this file to make the story change when you press on the choice buttons.
+// ✅ Step 24 - Run the app and try to figure out what code you need to add to this file to make the story change when you press on the choice buttons.
 
-//DONE: Step 29 - Run the app and test it against the Story Outline to make sure you've completed all the steps. The code for the completed app can be found here: https://github.com/londonappbrewery/destini-challenge-completed/
+// ✅ Step 29 - Run the app and test it against the Story Outline to make sure you've completed all the steps. The code for the completed app can be found here: https://github.com/londonappbrewery/destini-challenge-completed/
